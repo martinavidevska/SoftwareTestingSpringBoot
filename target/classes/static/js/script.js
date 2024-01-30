6f2c4d8385d0fdd3942fe71dc369c99f6d96208f
MartinaVidevska <92816280+M3tina@users.noreply.github.com>
2024-01-30 12:01:15 +0100

    add targer

---
+++ target/classes/static/js/script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * navbar toggle
+ */
+
+const overlay = document.querySelector("[data-overlay]");
+const navbar = document.querySelector("[data-navbar]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+const vehicleImageElement = document.getElementById('vehicleImage');
+const vehicleImageLink = "${vehicle.getPictureLink()}"; // Replace with your actual method or variable
+
+// Set the image source
+vehicleImageElement.src = vehicleImageLink;
+console.log("Image Link:", vehicleImageLink);
+const navToggleFunc = function () {
+    navToggleBtn.classList.toggle("active");
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+}
+
+navToggleBtn.addEventListener("click", navToggleFunc);
+overlay.addEventListener("click", navToggleFunc);
+
+for (let i = 0; i < navbarLinks.length; i++) {
+    navbarLinks[i].addEventListener("click", navToggleFunc);
+}
+
+
+
+/**
+ * header active on scroll
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+    window.scrollY >= 10 ? header.classList.add("active")
+        : header.classList.remove("active");
+});
